e43f8fb3f1610f752c13c20c9c6eb47250a8822b
sujal <sujalkhakhi402@gmail.com>
2026-01-31 19:55:32 +0530

    Chore : Token persistent resolve

--- api/auth/authController.js
+++ api/auth/authController.js
@@ -86,7 +86,7 @@ export const Login = async (req , res) => {
             const cookieOptions = {
               httpOnly: true,
               secure: process.env.NODE_ENV === 'production',
-              sameSite: 'strict',
+              // sameSite: 'strict',
               maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
             };
 
